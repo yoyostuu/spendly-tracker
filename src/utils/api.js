2d4386471667b565a156ee37ac53d528102d65fe
yoyostuu <12ananyamoharana34@gmail.com>
2026-07-07 15:54:10 +0530

    Configure API_BASE dynamically for production Vercel servers

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,6 @@
-const API_BASE = 'http://127.0.0.1:3001/api';
+const API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+  ? 'http://127.0.0.1:3001/api'
+  : '/api';
 
 export const api = {
   async register(email, password, name) {
